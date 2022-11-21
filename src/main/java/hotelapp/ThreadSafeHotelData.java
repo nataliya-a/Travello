130ffefcab7512827828b9d150e99bdefc02cfc0
nataliya-a <nkarmarkar@dons.usfca.edu>
2022-11-21 00:58:58 -0800

    ThreadSafeHotelData imported from previous projects

---
+++ src/main/java/hotelapp/ThreadSafeHotelData.java
@@ -0,0 +1,99 @@
+package hotelapp;
+
+import hotelapp.Hotel;
+import hotelapp.HotelData;
+
+import java.util.List;
+import java.util.TreeMap;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ThreadSafeHotelData extends HotelData {
+
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+
+    /** given a list of hotels, add them to the map */
+    public String getHotelData(String strHotelId) {
+        lock.readLock().lock();
+        try {
+            return super.getHotelData(strHotelId);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    /** given a hotelId, return the hotel */
+    public Hotel getHotelObject(String strHotelId) {
+        lock.readLock().lock();
+        try {
+            return super.getHotelObject(strHotelId);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    /** given a list of hotels, add them to the map */
+    public void addHotels(Hotel[] hotels) {
+        lock.writeLock().lock();
+        try {
+            super.addHotels(hotels);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    /** returns a list of all hotel IDs */
+
+    public String[] getAllHotelIDs() {
+        lock.readLock().lock();
+        try {
+            return super.getAllHotelIDs();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+
+    /** returns an hotelMap */
+    public TreeMap<String, Hotel> getHotelsMap() {
+        lock.readLock().lock();
+        try {
+            return super.getHotelsMap();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    /** given a list of hotels, add them to the map */
+
+    public void hotelMapperNew(List<Hotel> hotels) {
+        lock.writeLock().lock();
+        try {
+            super.hotelMapperNew(hotels);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    /** given a hotelId, return the hotel */
+    public String getHotelDataNew(String strHotelId) {
+        lock.readLock().lock();
+        try {
+            return super.getHotelDataNew(strHotelId);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    /** given a hotelId, return the hotel object */
+    public Hotel getHotelObjectNew(String strHotelId) {
+        lock.readLock().lock();
+        try {
+            return super.getHotelObjectNew(strHotelId);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+
+}
