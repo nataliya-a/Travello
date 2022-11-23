4cb95e76450c5c0e4c29f8438a8f7f244fac29bd
nataliya-a <nkarmarkar@dons.usfca.edu>
2022-11-23 15:31:35 -0800

    LoginServlet.java and login.html created

--- src/main/java/hotelapp/LoginServlet.java
+++ src/main/java/hotelapp/LoginServlet.java
@@ -12,122 +12,80 @@ import java.io.StringWriter;
 import java.util.Map;
 
 
-@SuppressWarnings("serial")
+
+
+/** A servlet that handles login requests.
+ */
 public class LoginServlet extends HttpServlet {
 
+    /**
+     * This method is called when a GET request is sent to the /login URL.
+     * This method should display the login page.
+     */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        String username = request.getParameter("username");
-//        System.out.println("username: " + username);
-//        username = StringEscapeUtils.escapeHtml4(username);
-//        System.out.println();
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
         PrintWriter out = response.getWriter();
 
-
         Cookie[] cookies = request.getCookies();
 
-        if (cookies != null ) { //|| request.getParameter("username") != null
+        if (cookies != null ) {
             for (Cookie cookie : cookies) {
                 if (cookie.getName().equals("username")) {
                     response.sendRedirect("/search");
-//                    out.println("<h3>Welcome, " + cookie.getValue() + "</h3>");
                 }
             }
         } else {
             System.out.println();
-            response.setContentType("text/html");
-            response.setStatus(HttpServletResponse.SC_OK);
-            out.printf("<html>%n%n");
-            out.printf("<head><title>%s</title></head>%n", "Form");
-            out.printf("<body>%n");
+            VelocityEngine ve = (VelocityEngine) request.getServletContext().getAttribute("templateEngine");
+            VelocityContext context = new VelocityContext();
+            Template template = ve.getTemplate("templates/login.html");
+            StringWriter writer = new StringWriter();
+            template.merge(context, writer);
+            out.println(writer);
 
-            printForm(request, response);
-
-            out.printf("%n</body>%n");
-            out.printf("</html>%n");
         }
 
-//        if (request.getParameter("username") == null) {
-//            out.println("Please login below.</p>");
-//            System.out.println();
-//            response.setContentType("text/html");
-//            response.setStatus(HttpServletResponse.SC_OK);
-//            out.printf("<html>%n%n");
-//            out.printf("<head><title>%s</title></head>%n", "Form");
-//            out.printf("<body>%n");
-//
-//            printForm(request, response);
-//
-//            out.printf("%n</body>%n");
-//            out.printf("</html>%n");
-//        }
-//        else  {
-//            // already logged in
-//            System.out.println();
-//            response.setContentType("text/html");
-//            response.setStatus(HttpServletResponse.SC_OK);
-//            PrintWriter pw = response.getWriter();
-//            Cookie[] cookies = request.getCookies();
-////            for (Cookie cookie : cookies) {
-////                System.out.println(cookie.getName() + " " + cookie.getValue());
-////
-//            if (cookies != null) {
-//                for (Cookie cookie : cookies) {
-//                    if (cookie.getName().equals("username")) {
-//                        pw.println("Welcome, " + cookie.getValue() + "!");
-//                    }
-//                }
-////                out.println("Login successful. Welcome, " + username);
-//            }
-//
-//        }
     }
 
+
+    /**
+     * This method is called when a POST request is sent to the /login URL.
+     * This method should handle the login request and display the search page.
+     */
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
 
         String username = request.getParameter("username");
+
         username = StringEscapeUtils.escapeHtml4(username);
-        Cookie userCookie = new Cookie("username", username);
-        userCookie.setMaxAge(30*60);
-        response.addCookie(userCookie);
 
         String pass = request.getParameter("pass");
 
         DatabaseHandler dbHandler = DatabaseHandler.getInstance();
         boolean flag = dbHandler.authenticateUser(username, pass);
+
         if (flag) {
-//            Cookie userCookie = new Cookie("username", username);
-//            userCookie.setMaxAge(30*60);
-//            response.addCookie(userCookie);
+            Cookie userCookie = new Cookie("username", username);
+            userCookie.setMaxAge(30*60);
+            response.addCookie(userCookie);
+            response.sendRedirect("/search?username=" + username);
 
-//            response.sendRedirect("/hotels");
-//            response.addCookie(new Cookie("username", username));
-            response.sendRedirect("/login?username=" + username);
         }
-        else
-            response.sendRedirect("/login");
+        else {
+            PrintWriter out = response.getWriter();
+            VelocityEngine ve = (VelocityEngine) request.getServletContext().getAttribute("templateEngine");
+            VelocityContext context = new VelocityContext();
+            context.put("error", "Invalid username or password. Try again.");
+            Template template = ve.getTemplate("templates/login.html");
+            StringWriter writer = new StringWriter();
+            template.merge(context, writer);
+            out.println(writer);
+        }
 
     }
 
-    private static void printForm(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        PrintWriter out = response.getWriter();
-//
-//        out.printf("<form method=\"post\" action=\"%s\">%n", request.getServletPath());
-//        out.printf("Enter your username:<br><input type=\"text\" name=\"username\"><br>");
-//        out.printf("Enter your password:<br><input type=\"password\" name=\"pass\"><br>");
-//        out.printf("<p><input type=\"submit\" value=\"Enter\"></p>\n%n");
-//        out.printf("</form>\n%n");
-        PrintWriter out = response.getWriter();
-        VelocityEngine ve = (VelocityEngine) request.getServletContext().getAttribute("templateEngine");
-        VelocityContext context = new VelocityContext();
-        Template template = ve.getTemplate("templates/login.html");
-        StringWriter writer = new StringWriter();
-        template.merge(context, writer);
-        out.println(writer);
-    }
 }
