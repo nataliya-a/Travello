26de50c924842df541f3f65e3656cb9d8fd017b3
nataliya-a <nkarmarkar@dons.usfca.edu>
2022-12-03 21:09:47 -0800

    allow user to favorite a hotel

---
+++ src/main/java/servlets/AddFavServlet.java
@@ -0,0 +1,44 @@
+package servlets;
+
+import hotelapp.DatabaseHandler;
+import hotelapp.utils.UserManager;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/** A POST only servlet that handles hotel favoring requests.
+ */
+public class AddFavServlet extends HttpServlet {
+
+    /**
+     * This method is called when a POST request is sent to the /addfav URL.
+     * This method should handle the favoring request and display the hotel page.
+     */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        DatabaseHandler dbHandler = DatabaseHandler.getInstance();
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        if (!UserManager.isLoggedIn(request.getCookies())) {
+            response.sendRedirect("/login");
+            return;
+        }
+        String username = UserManager.getUsername(request.getCookies());
+
+        String action = request.getParameter("action");
+        if (action != null) {
+            dbHandler.clearFavHotelHistory(username);
+            response.sendRedirect("/myprofile");
+        } else {
+            String hotelId = request.getParameter("hotelId");
+            dbHandler.insertFavHotel(username, hotelId);
+            response.sendRedirect("/hotels?hotelId=" + hotelId);
+
+        }
+
+    }
+}
