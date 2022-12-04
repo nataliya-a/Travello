2473c098c7e411a53053365330d70fb766a7046a
nataliya-a <nkarmarkar@dons.usfca.edu>
2022-12-03 22:47:01 -0800

    clean up of config files

--- src/main/java/hotelapp/ParseArgs.java
+++ src/main/java/hotelapp/ParseArgs.java
@@ -2,6 +2,7 @@ package hotelapp;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 /** ParseArgs class parses the command line arguments and returns the values. */
 public class ParseArgs {
@@ -15,16 +16,30 @@ public class ParseArgs {
             argsMap.put(args[i], args[i + 1]);
         }
 
+        dbTables();
+        DatabaseHandler dbHandler = DatabaseHandler.getInstance();
         HotelParser hp = new HotelParser();
         List<Hotel> hotels = hp.parseHotelDataNew(argsMap.get("-hotels"));
 
         hd.hotelMapperNew(hotels);
-
+        if (dbHandler.checkHotelsTableEmpty() == 0) {
+            System.out.println("Inserting hotels into database...");
+            hd.addHotelToDB(hotels);
+        }
 
         ReviewParser rp = new ReviewParser();
         List<String> paths = rp.filePath(argsMap.get("-reviews"));
         List<Review> reviews = rp.parseReviewsFiles(paths);
+        System.out.println(reviews.size());
         rm.addReviews(reviews);
+        if (dbHandler.checkReviewsTableEmpty() == 0) {
+            System.out.println("Inserting reviews into database...");
+            rm.addReviewsToDB(reviews);
+        }
+
+
+
+
 
     }
 
@@ -43,6 +58,27 @@ public class ParseArgs {
     public static ThreadSafeReviewMapper getReviewMapper() {
         return rm;
     }
+
+
+    /**
+     * Creates the database tables if they don't exist already.
+     */
+    public static void dbTables() {
+        DatabaseHandler dbHandler = DatabaseHandler.getInstance();
+        if (dbHandler.checkTable("hotels") != 1) {
+            dbHandler.createHotelsTable();
+        }
+        if (dbHandler.checkTable("reviews") != 1) {
+            dbHandler.createReviewsTable();
+        }
+        if (dbHandler.checkTable("expedia") != 1) {
+            dbHandler.createExpediaTable();
+        }
+        if (dbHandler.checkTable("favhotels") != 1) {
+            dbHandler.createFavHotelsTable();
+        }
+    }
+
 
 
 }
