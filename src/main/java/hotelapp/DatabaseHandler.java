6cb57b0ef48daa30d9f637652a04cdab7ee4836d
nataliya-a <nkarmarkar@dons.usfca.edu>
2022-12-03 16:14:16 -0800

    Build database handlers needed for the project

--- src/main/java/hotelapp/DatabaseHandler.java
+++ src/main/java/hotelapp/DatabaseHandler.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.sql.*;
-import java.util.Properties;
-import java.util.Random;
+import java.util.*;
 
 /**
  * A class that handles all database operations.
@@ -211,6 +210,451 @@ public class DatabaseHandler {
             throw new RuntimeException(e);
         }
     }
+
+    /**
+     * Checks if the table exists in the database.
+     */
+
+    public int checkTable(String tableName) {
+        PreparedStatement statement;
+        int count = 0;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+
+            statement = connection.prepareStatement(PreparedStatements.CHECK_SQL_TABLE);
+            statement.setString(1, tableName);
+            ResultSet results = statement.executeQuery();
+            if (results.next()) {
+                count = Integer.parseInt(results.getString("count(*)"));
+            }
+            return count;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Creates a new hotels table in the database.
+     */
+
+    public void createHotelsTable() {
+        Statement statement;
+        try (Connection dbConnection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = dbConnection.createStatement();
+            statement.executeUpdate(PreparedStatements.CREATE_HOTELS_TABLE);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /**
+     * Creates a new reviews table in the database.
+     */
+    public void createReviewsTable() {
+        Statement statement;
+        try (Connection dbConnection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = dbConnection.createStatement();
+            statement.executeUpdate(PreparedStatements.CREATE_REVIEWS_TABLE);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /**
+     * Inserts a new hotel into the hotels table.
+     */
+
+    //String hotelName, String hotelID, String address, String city, String state, String lat, String lon
+    public void insertHotel(String hotelId, String hotelName, String address, String city, String state, String lat, String lon) {
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.INSERT_HOTEL_SQL);
+            statement.setString(1, hotelId);
+            statement.setString(2, hotelName);
+            statement.setString(3, address);
+            statement.setString(4, city);
+            statement.setString(5, state);
+            statement.setString(6, lat);
+            statement.setString(7, lon);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+
+    /**
+     * Inserts a new review into the reviews table.
+     */
+    public void insertReview(String hotelId, String reviewId, String reviewTitle, String reviewText, String username, String reviewRating, String reviewDate) {
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.INSERT_REVIEW_SQL);
+            statement.setString(1, hotelId);
+            statement.setString(2, reviewId);
+            statement.setString(3, reviewTitle);
+            statement.setString(4, reviewText);
+            statement.setString(5, username);
+            statement.setString(6, reviewRating);
+            statement.setString(7, reviewDate);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+
+        /**
+         * Checks if the table is empty.
+         */
+    public int checkHotelsTableEmpty() {
+        PreparedStatement statement;
+        int count = -1;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+
+            statement = connection.prepareStatement(PreparedStatements.CHECK_HOTELS_TABLE_EMPTY);
+            ResultSet results = statement.executeQuery();
+            if (results.next()) {
+                count = Integer.parseInt(results.getString("count(*)"));
+            }
+            return count;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int checkReviewsTableEmpty() {
+        PreparedStatement statement;
+        int count = -1;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+
+            statement = connection.prepareStatement(PreparedStatements.CHECK_REVIEWS_TABLE_EMPTY);
+            ResultSet results = statement.executeQuery();
+            if (results.next()) {
+                count = Integer.parseInt(results.getString("count(*)"));
+            }
+            return count;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Gets the hotelName from the hotels table.
+     */
+    public Map<String, String> getHotelName(String keyword) {
+        keyword = "%" + keyword.toUpperCase() + "%";
+        System.out.println(keyword);
+//        List<String> hotelName = new ArrayList<>();
+        Map<String, String> matches = new HashMap<>();
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+
+            statement = connection.prepareStatement(PreparedStatements.GET_HOTEL_SQL);
+            statement.setString(1, keyword);
+            System.out.println(statement);
+            ResultSet results = statement.executeQuery();
+
+            while (results.next()) {
+                matches.put(results.getString("hotelId"), results.getString("hotelName"));
+
+            }
+            return matches;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Gets the details of hotels matching the hotelID.
+     */
+    public Map<String, String> getHotelDetails(String hotelId) {
+        Map<String, String> details = new HashMap<>();
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+
+            statement = connection.prepareStatement(PreparedStatements.GET_HOTEL_DETAILS_SQL);
+            statement.setString(1, hotelId);
+            System.out.println(statement);
+            ResultSet results = statement.executeQuery();
+
+            if (results.next()) {
+                details.put("hotelID", results.getString("hotelID"));
+                details.put("hotelName", results.getString("hotelName"));
+                details.put("address", results.getString("address"));
+                details.put("city", results.getString("city"));
+                details.put("state", results.getString("state"));
+                details.put("lat", results.getString("lat"));
+                details.put("lon", results.getString("lon"));
+            }
+            return details;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Gets the reviews of hotels matching the hotelID.
+     */
+    public List<Map<String, String>> getHotelReviews(String hotelId) {
+        List<Map<String, String>> reviews = new ArrayList<>();
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+
+            statement = connection.prepareStatement(PreparedStatements.GET_HOTEL_REVIEWS_SQL);
+            statement.setString(1, hotelId);
+            System.out.println(statement);
+            ResultSet results = statement.executeQuery();
+
+            while (results.next()) {
+                Map<String, String> review = new HashMap<>();
+                review.put("hotelID", results.getString("hotelID"));
+                review.put("reviewID", results.getString("reviewID"));
+                review.put("reviewTitle", results.getString("title"));
+                review.put("reviewText", results.getString("reviewText"));
+                review.put("username", results.getString("username"));
+                review.put("reviewRating", results.getString("ratings"));
+                review.put("reviewDate", results.getString("reviewSubmissionTime"));
+                reviews.add(review);
+            }
+            return reviews;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Updates the review if the hotelID and username matches.
+     */
+    public void updateReview(String reviewTitle, String reviewText, String reviewRating, String hotelId, String username) {
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.UPDATE_REVIEW_SQL);
+            statement.setString(1, reviewTitle);
+            statement.setString(2, reviewText);
+            statement.setString(3, reviewRating);
+            statement.setString(4, hotelId);
+            statement.setString(5, username);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /**
+     * Deletes the review if the hotelID and username matches.
+     */
+    public void deleteReview(String reviewId) {
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.DELETE_REVIEW_SQL);
+            statement.setString(1, reviewId);
+            System.out.println(statement);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /**
+     * Gets the review from reviews when the hotelID and username matches.
+     */
+    public Map<String, String> getReview(String hotelId, String username) {
+        Map<String, String> review = new HashMap<>();
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+
+            statement = connection.prepareStatement(PreparedStatements.GET_REVIEW_SQL);
+            statement.setString(1, hotelId);
+            statement.setString(2, username);
+            System.out.println(statement);
+            ResultSet results = statement.executeQuery();
+
+            if (results.next()) {
+                review.put("hotelID", results.getString("hotelID"));
+                review.put("reviewId", results.getString("reviewID"));
+                review.put("reviewTitle", results.getString("title"));
+                review.put("reviewText", results.getString("reviewText"));
+                review.put("username", results.getString("username"));
+                review.put("reviewRating", results.getString("ratings"));
+                review.put("reviewDate", results.getString("reviewSubmissionTime"));
+            }
+            return review;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Inserts the expedia link into the user's expedia table.
+     */
+    public void insertExpediaTable(String username, String expediaLink) {
+        if (linkExistsForUser(username, expediaLink)) return;
+
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.INSERT_EXPEDIA_SQL);
+            statement.setString(1, username);
+            statement.setString(2, expediaLink);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+
+    /** Clear the expedia link history for a given user. */
+    public void clearExpediaHistory(String username) {
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.DELETE_EXPEDIA_LINKS_FOR_USERNAME);
+            statement.setString(1, username);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /** Get the expedia link history for a given user. */
+    public List<String> getExpediaLinksForUser(String username) {
+        List<String> links = new ArrayList<>();
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.GET_EXPEDIA_LINKS_FOR_USERNAME);
+            statement.setString(1, username);
+            ResultSet results = statement.executeQuery();
+            while (results.next()) {
+                links.add(results.getString("link"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return links;
+    }
+
+    /**
+     * Check if the link already exists for user;
+     *
+     */
+    public boolean linkExistsForUser(String username, String expediaLink) {
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.EXPEDIA_LINK_EXIST_FOR_USERNAME);
+            statement.setString(1, username);
+            statement.setString(2, expediaLink);
+            System.out.println(statement);
+            ResultSet results = statement.executeQuery();
+            if (results.next()) {
+                int count = Integer.parseInt(results.getString("count(*)"));
+                if (count > 0) {
+                    return true;
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return false;
+    }
+
+
+    /**
+     * Creates the expedia table.
+     */
+    public void createExpediaTable() {
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.CREATE_EXPEDIA_TABLE);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+    /** Create the favhotels table. */
+    public void createFavHotelsTable() {
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.CREATE_FAV_HOTEL_TABLE);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /** Get a list of hotels that the username favored. */
+    public List<Hotel> getFavHotels(String username) {
+        List<Hotel> favHotels = new ArrayList<>();
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.GET_FAV_HOTELS_FOR_USERNAME);
+            statement.setString(1, username);
+            ResultSet results = statement.executeQuery();
+            while (results.next()) {
+                String hotelId = results.getString("hotelID");
+                Map<String, String> hotelDetails = getHotelDetails(hotelId);
+                Hotel hotel = new Hotel(
+                        hotelDetails.get("hotelName"),
+                        hotelDetails.get("hotelID"),
+                        hotelDetails.get("address"),
+                        hotelDetails.get("city"),
+                        hotelDetails.get("state"),
+                        hotelDetails.get("lat"),
+                        hotelDetails.get("lon")
+                        );
+                favHotels.add(hotel);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return favHotels;
+    }
+
+    /** Insert a favorite hotel for a user. */
+    public void insertFavHotel(String username, String hotelId) {
+        if (favHotelExistsForUser(username, hotelId)) return;
+
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.INSERT_FAV_HOTEL_SQL);
+            statement.setString(1, username);
+            statement.setString(2, hotelId);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    /** Clear all favored hotels. */
+    public void clearFavHotelHistory(String username) {
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.DELETE_FAV_HOTELS_FOR_USERNAME);
+            statement.setString(1, username);
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+
+
+    /** Check if the favorite hotel already exists for user. */
+    public boolean favHotelExistsForUser(String username, String hotelId) {
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(uri, config.getProperty("username"), config.getProperty("password"))) {
+            statement = connection.prepareStatement(PreparedStatements.FAV_HOTEL_EXIST_FOR_USERNAME);
+            statement.setString(1, username);
+            statement.setString(2, hotelId);
+            ResultSet results = statement.executeQuery();
+            if (results.next()) {
+                int count = Integer.parseInt(results.getString("count(*)"));
+                if (count > 0) {
+                    return true;
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return false;
+    }
+
 
 }
 
--- src/main/java/hotelapp/PreparedStatements.java
+++ src/main/java/hotelapp/PreparedStatements.java
@@ -30,5 +30,173 @@ public class PreparedStatements {
                     "WHERE username = ?";
 
 
+    /** Used to create the hotels table. */
+    public static final String CREATE_HOTELS_TABLE =
+            "CREATE TABLE hotels (" +
+                    "hotelID VARCHAR(225) PRIMARY KEY, " +
+                    "hotelName VARCHAR(225) NOT NULL, " +
+                    "address VARCHAR(225) NOT NULL, " +
+                    "city VARCHAR(128) NOT NULL, " +
+                    "state CHAR(2) NOT NULL, " +
+                    "lat VARCHAR(25), " +
+                    "lon VARCHAR(25));";
+
+
+    /** Used to insert a new hotel into the hotels table. */
+    public static final String INSERT_HOTEL_SQL =
+            "INSERT INTO hotels " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+
+    /** Used to check if a table already exists in the database. */
+    public static final String CHECK_SQL_TABLE =
+            "SELECT count(*) " +
+                    "FROM information_schema.tables " +
+                    "WHERE table_schema = 'user018' " +
+                    "AND table_name = ?";
+
+    /** Create the favhotels table. */
+    public static final String CREATE_FAV_HOTEL_TABLE =
+            "CREATE TABLE favhotels (" +
+                    "id INTEGER AUTO_INCREMENT PRIMARY KEY, " +
+                    "username VARCHAR(225), " +
+                    "hotelID VARCHAR(125));";
+
+    /** Used to create the reviews table. */
+    public static final String CREATE_REVIEWS_TABLE =
+            "CREATE TABLE reviews (" +
+                    "id INTEGER AUTO_INCREMENT PRIMARY KEY, " +
+                    "hotelID VARCHAR(125), " +
+                    "reviewID VARCHAR(225), " +
+                    "title VARCHAR(225), " +
+                    "reviewText VARCHAR(3000), " +
+                    "username VARCHAR(225), " +
+                    "ratings VARCHAR(25), " +
+                    "reviewSubmissionTime VARCHAR(225));";
+
+
+    /** Used to insert a new review into the reviews table. */
+    public static final String INSERT_REVIEW_SQL =
+            "INSERT INTO reviews (hotelID, reviewID, title, reviewText, username, ratings, reviewSubmissionTime) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+
+    /** Used to check if the existing hotels table is empty. */
+    public static final String CHECK_HOTELS_TABLE_EMPTY =
+            "SELECT count(*) " +
+                    "FROM hotels;";
+
+    /** Used to check if the existing reviews table is empty. */
+    public static final String CHECK_REVIEWS_TABLE_EMPTY =
+            "SELECT count(*) " +
+                    "FROM reviews;";
+
+
+    /**
+     * Gets the matching hotel from the hotels table.
+     */
+    //select hotelName from hotels where upper(hotelName) LIKE '%?%';
+    public static final String GET_HOTEL_SQL =
+            "SELECT hotelID, hotelName " +
+                    "FROM hotels " +
+                    "WHERE upper(hotelName) LIKE ?;";
+
+    /**
+     * Gets the details of hotels matching the hotelID.
+     */
+    public static final String GET_HOTEL_DETAILS_SQL =
+            "SELECT * " +
+                    "FROM hotels " +
+                    "WHERE hotelID = ?;";
+
+    /**
+     * Gets the reviews of hotels matching the hotelID and order by reviewSubmissionTime.
+     */
+    public static final String GET_HOTEL_REVIEWS_SQL =
+            "SELECT * " +
+                    "FROM reviews " +
+                    "WHERE hotelID = ? " +
+                    "ORDER BY reviewSubmissionTime DESC;";
+
+    /**
+     * Updates the review if the hotelID and username matches.
+     */
+    public static final String UPDATE_REVIEW_SQL =
+            "UPDATE reviews " +
+                    "SET title = ?, reviewText = ?, ratings = ? " +
+                    "WHERE hotelID = ? AND username = ?;";
+
+    /**
+     * Deletes the review if the hotelID and username matches.
+     */
+    public static final String DELETE_REVIEW_SQL =
+            "DELETE FROM reviews " +
+                    "WHERE reviewID = ?;";
+
+    /**
+     * Gets the review from reviews when the hotelID and username matches.
+     */
+    public static final String GET_REVIEW_SQL =
+            "SELECT * " +
+                    "FROM reviews " +
+                    "WHERE hotelID = ? AND username = ?;";
+
+    /**
+     * Creates a new table for the user's expedia links.
+     */
+    public static final String CREATE_EXPEDIA_TABLE =
+            "CREATE TABLE expedia (" +
+                    "id INTEGER AUTO_INCREMENT PRIMARY KEY, " +
+                    "username VARCHAR(225), " +
+                    "link VARCHAR(520));";
+
+    /**
+     * Inserts the expedia link into the user's expedia table.
+     */
+    public static final String INSERT_EXPEDIA_SQL =
+            "INSERT INTO expedia (username, link) " +
+                    "VALUES (?, ?);";
+
+    /**
+     * Gets the expedia links from the user's expedia table.
+     */
+    public static final String EXPEDIA_LINK_EXIST_FOR_USERNAME =
+            "SELECT count(*) " +
+                    "FROM expedia " +
+                    "WHERE username = ? AND link = ?;";
+
+
+    /** Delete all links for user under expedia table. */
+    public static final String DELETE_EXPEDIA_LINKS_FOR_USERNAME =
+            "DELETE FROM expedia " +
+                    "WHERE username = ?;";
+
+
+    /** Gets all links for user under expedia table. */
+    public static final String GET_EXPEDIA_LINKS_FOR_USERNAME =
+            "SELECT link " +
+                    "FROM expedia " +
+                    "WHERE username = ?;";
+
+    /** Gets all the fav hotels for user under favhotels table. */
+    public static final String GET_FAV_HOTELS_FOR_USERNAME =
+            "SELECT favhotels.hotelID, hotels.hotelName FROM favhotels INNER JOIN hotels ON favhotels.hotelID=hotels.hotelID " +
+                    "WHERE favhotels.username = ?;";
+
+    /** Inserts the fav hotel into the user's favhotels table. */
+    public static final String INSERT_FAV_HOTEL_SQL =
+            "INSERT INTO favhotels (username, hotelID) " +
+                    "VALUES (?, ?);";
+
+    /** Checks if the fav hotel already exists in the user's favhotels table. */
+    public static final String FAV_HOTEL_EXIST_FOR_USERNAME =
+            "SELECT count(*) " +
+                    "FROM favhotels " +
+                    "WHERE username = ? AND hotelID = ?;";
+
+    /** Delete all links for user under favhotesl table. */
+    public static final String DELETE_FAV_HOTELS_FOR_USERNAME =
+            "DELETE FROM favhotels " +
+                    "WHERE username = ?;";
 
 }
