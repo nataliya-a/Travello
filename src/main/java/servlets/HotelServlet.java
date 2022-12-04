4cf4ce7bdab6fff61acbdc419fa1607c6df86817
nataliya-a <nkarmarkar@dons.usfca.edu>
2022-12-03 21:08:50 -0800

    build frontend for hotel page

--- src/main/java/servlets/HotelServlet.java
+++ src/main/java/servlets/HotelServlet.java
@@ -1,9 +1,6 @@
 package servlets;
 
-import hotelapp.Hotel;
-import hotelapp.Review;
-import hotelapp.ThreadSafeHotelData;
-import hotelapp.ThreadSafeReviewMapper;
+import hotelapp.*;
 import hotelapp.utils.UserManager;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -32,42 +29,43 @@ public class HotelServlet extends HttpServlet {
          * @throws IOException
          */
         @Override
-        public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            DatabaseHandler dbHandler = DatabaseHandler.getInstance();
             Double rating = 0.0;
             String hotelId = request.getParameter("hotelId");
+            System.out.println("hotelId: " + hotelId);
 
             response.setContentType("text/html");
             response.setStatus(HttpServletResponse.SC_OK);
             PrintWriter out = response.getWriter();
 
             if (!UserManager.isLoggedIn(request.getCookies())) {
-                out.println("<h1>You are not logged in!</h1>");
                 response.sendRedirect("/login");
                 return;
             }
             String username = UserManager.getUsername(request.getCookies());
 
+            Map<String, String> hotelDetails= dbHandler.getHotelDetails(hotelId);
+
             ThreadSafeHotelData hotelData = (ThreadSafeHotelData) request.getServletContext().getAttribute("hotelData");
-            Hotel hotel = hotelData.getHotelObjectNew(hotelId);
             ArrayList<LocalDate> myBookedDates = hotelData.getUserBookedDates(hotelId, username);
 
-            ThreadSafeReviewMapper reviewMapper = (ThreadSafeReviewMapper) request.getServletContext().getAttribute("reviewMapper");
-            TreeSet<Review> reviews = reviewMapper.getReviewObject(hotelId);
-
+            List<Map<String, String>> reviews = dbHandler.getHotelReviews(hotelId);
 
             if (reviews != null) {
-                for (Review review : reviews) {
-                    if (review.getRatingOverall() == "") {
+                for (Map<String, String> review : reviews) {
+                    if (review.get("reviewRating") == "") {
                         rating += 0.0;
                     } else {
-                        rating += Double.parseDouble(review.getRatingOverall());
+                        rating += Double.parseDouble(review.get("reviewRating"));
                     }
                 }
                 rating = rating / reviews.size();
             }
 
-            String city = hotel.getCity().replace(" ", "-");
-            String hotelName = hotel.getHotelName().replace(" ", "-");
+            String city = hotelDetails.get("city").replace(" ", "-");
+
+            String hotelName = hotelDetails.get("hotelName").replace(" ", "-");
             String expediaLink = "https://www.expedia.com/" + city +
                     "-Hotels-" + hotelName + ".h" + hotelId + ".Hotel-Information";
 
@@ -75,12 +73,17 @@ public class HotelServlet extends HttpServlet {
             VelocityEngine ve = (VelocityEngine) request.getServletContext().getAttribute("templateEngine");
             VelocityContext context = new VelocityContext();
             Template template = ve.getTemplate("templates/hotel.html");
-            context.put("hotel", hotel);
+            context.put("hotel", hotelDetails);
             context.put("reviews", reviews);
-            context.put("rating", rating);
+            context.put("isFavoredHotel", dbHandler.favHotelExistsForUser(username, hotelId));
+            System.out.println(reviews);
+            Formatter formatter = new Formatter();
+            formatter.format("%.2f", rating);
+            context.put("rating", formatter.toString());
             context.put("expediaLink", expediaLink);
             context.put("username", username);
             context.put("myBookedDates", myBookedDates);
+            context.put("currentPage", request.getParameter("page") == null ? 1 : Integer.parseInt(request.getParameter("page")));
             StringWriter writer = new StringWriter();
             template.merge(context, writer);
             out.println(writer);
@@ -91,32 +94,47 @@ public class HotelServlet extends HttpServlet {
          */
         @Override
         public void doPost (HttpServletRequest request, HttpServletResponse response) throws  IOException {
-            System.out.println("hotel servlet post");
             response.setContentType("text/html");
             response.setStatus(HttpServletResponse.SC_OK);
-            PrintWriter out = response.getWriter();
-            String hotelId = request.getParameter("hotelId");
-            String title = request.getParameter("title");
-            String reviewText = request.getParameter("review");
+
+            DatabaseHandler dbHandler = DatabaseHandler.getInstance();
             String username = UserManager.getUsername(request.getCookies());
+
+            String body = request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
+            System.out.println(body);
+            String[] parameters = body.split("&");
+            Map<String, String> params = new HashMap<>();
+            for (String param : parameters) {
+                String[] keyValue = param.split("=");
+                if (keyValue.length == 2) {
+                    params.put(keyValue[0], keyValue[1]);
+                }
+            }
+
+            if (params.get("link") != null) {
+                dbHandler.insertExpediaTable(username, params.get("link"));
+            }
+
+
+            String ratingString = "0";
+            System.out.println("hotel servlet post");
+
+            String hotelId = params.get("hotelId");
+            String title =params.get("title");
+            String reviewText = params.get("review");
+            ratingString = params.get("rating");
+
 
             if (title != null && reviewText != null) {
                 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd");
                 String date = dtf.format(LocalDate.now());
                 date = date.replace("/", "-");
-                date = date + "T00:00:00Z";
-                Review newReview = new Review(hotelId, reviewText,  username, title, UUID.randomUUID().toString(), "", date);
-                System.out.println(newReview);
-                ThreadSafeReviewMapper reviewMapper = (ThreadSafeReviewMapper) request.getServletContext().getAttribute("reviewMapper");
-                TreeSet<Review> reviews = reviewMapper.getReviewObject(hotelId);
-                if (reviews == null) {
-                    List<Review> reviewList = new ArrayList<>();
-                    reviewList.add(newReview);
-                    reviewMapper.addReviews(reviewList);
-                } else {
-                    reviewMapper.addNewReview(hotelId,newReview);
-                }
+                dbHandler.insertReview(hotelId, UUID.randomUUID().toString(), title, reviewText, username, ratingString, date);
             }
+
+
             response.sendRedirect("/hotels?hotelId=" + hotelId);
+
         }
+
 }
